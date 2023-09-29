e84e5990163368af6748984cacc59a30d372e24a
WillBoudreau <williamboudreau804@gmail.com>
2023-09-29 13:55:51 -0400

    Revert "Added the basic Dialogue scripts into the game"
    This reverts commit 109dba22723c825d76e6eee3688658523171094c.

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueInterations : MonoBehaviour
+{
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        nameText.text = dialogue.name;
+
+        sentences = new Queue<string>();
+    }
+    public void StartDialogue (Dialogue dialogue)
+    {
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences )
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+    void EndDialogue()
+    {
+        Debug.Log("End of Conversation");
+    }
+}
