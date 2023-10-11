4bf8c888fdea4406a0ad1612cbf8113c62d0791f
WillBoudreau <williamboudreau804@gmail.com>
2023-10-11 15:17:56 -0300

    fixed Colliders + began to work on text speed October 11th 2023

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Dialogue 
+public class Dialogue
 {
+    float textSpeed = 0.5f;
+
     public string name;
 
-    [TextArea(3,10)]
+    [TextArea(3, 10)]
     public string[] sentences;
 }
